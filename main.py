dcfc21b844a003773e71a0ee3e2eaa1d26977f12
Sal052 <133681528+Sal052@users.noreply.github.com>
2023-05-19 15:54:42 +0000

    Stuffs

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 print("hello world!")
 print("goodbye world!")
 print("let me push")
+
+import requests
+
+def trace(*args):
+  print (*args)
+  pass
+
+barcode = input("Give me barcode:")
+URL = "https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json"
+print (URL)
+
+trace ("Calling", URL)
+response = requests.get(URL)
+response.raise_for_status()
+data = response.json()
+
+trace ("\nText returned:", response.text)
+
+trace("\nHere are all the key/value paurs in the JSON response:")
+for key, value in data.items():
+  trace (key, ":", value)
+
+print(f"Here's a _____ for you")
