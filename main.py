1b2c4654f7902b817d2cdeb4356534443e48428d
s-ibrahima <133246633+s-ibrahima@users.noreply.github.com>
2023-06-02 15:30:44 +0000

    Final Commit

--- main.py
+++ main.py
@@ -18,16 +18,13 @@ data = response.json()
 
 #trace ("\nText returned:", response.text)
 
-trace("\nHere are all the key/value paurs in the JSON response:")
-for key, value in data.items():
-  trace ("\n" + key)
-
-print(f"Always check the allergen information before consuming a new product for your wellbeing.")
+print("\n" + f"Always check the allergen information before consuming a new product for your wellbeing.")
 
 #Creates variable storing the product information
 product = data["product"]
 
 #Product information is scanned for allergen information and returns it
+print("\n")
 print (product["allergens_hierarchy"])
 
 
